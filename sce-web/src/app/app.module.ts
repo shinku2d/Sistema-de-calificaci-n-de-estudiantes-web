7b6b903ad4a7e282c9b2b9450c69610ef6555a54
Eliseo Loría <shinku2d@gmail.com>
2019-03-06 18:41:18 -0600

    Componentes
    Se agregan los componentes principales del proyecto

--- sce-web/src/app/app.module.ts
+++ sce-web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
