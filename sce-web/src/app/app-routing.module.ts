7c341c5e2a68bf8e46191524f7d707c9b1f0f307
Eliseo Loría <shinku2d@gmail.com>
2019-03-07 07:49:33 -0600

    Rutas
    Se configuran las rutas del proyecto

--- sce-web/src/app/app-routing.module.ts
+++ sce-web/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { SearchComponent } from './components/search/search.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'students', component: StudentsListComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'search' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
